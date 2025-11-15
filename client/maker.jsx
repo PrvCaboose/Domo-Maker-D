24649f87638942ba27a0175e3b71cdd72672ac6d
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2025-11-15 14:37:53 -0500

    filepath fix

--- client/maker.jsx
+++ client/maker.jsx
@@ -59,7 +59,7 @@ const DomoList = (props) => {
   const domoNodes = domos.map(domo => {
     return (
       <div key={domo.id} className='domo'>
-        <img src="./assets/img/domoFace.jpeg" alt="domo face" className='domoFace' />
+        <img src="/assets/img/domoface.jpeg" alt="domo face" className='domoFace' />
         <h3 className='domoName'>Name: {domo.name}</h3>
         <h3 className='domoAge'>Age: {domo.age}</h3>
       </div>
